c4ad18261a2346ee26feb5c389ae2bf23df58d60
stella <sehyunny6@gmail.com>
2020-03-25 00:23:15 +0900

    Cascading sub items

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -23,16 +23,15 @@ public class Order {
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL) // order 저장시 order items 가 함께 저장될 수 있도록
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne(fetch = LAZY)
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
     private LocalDateTime orderDate;
 
     private OrderStatus status; // 주문 상태 [ORDER, CANCEL]
-
 
 }
