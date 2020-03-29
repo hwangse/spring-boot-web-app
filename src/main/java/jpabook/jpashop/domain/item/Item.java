ff9f141b8e4cf1fa591cc2d35e0b31d13af8d6f6
stella <sehyunny6@gmail.com>
2020-03-29 12:18:16 +0900

    Create item repository and service

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
 import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -27,4 +28,24 @@ public abstract class Item{
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // business logic
+
+    /**
+     * 재고 추가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * 재고 감소
+     */
+    public void removeStock(int quantity){
+        int remain = this.stockQuantity - quantity;
+        if(remain < 0){
+            throw new NotEnoughStockException("상품의 재고가 부족합니다.");
+        }
+        this.stockQuantity = remain;
+    }
 }
---
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+    private final ItemRepository itemRepository;
+
+    // 저장
+    public void saveItem(Item item){
+        itemRepository.save(item);
+    }
+
+    // 조회
+    public Item findOne(Long id){
+        return itemRepository.findOne(id);
+    }
+
+    public List<Item> findItems(){
+        return itemRepository.findAll();
+    }
+}
