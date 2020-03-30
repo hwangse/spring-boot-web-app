8a40d52ecf56c9b289baa063b59655b0b322a273
stella <sehyunny6@gmail.com>
2020-03-30 23:25:07 +0900

    Update address class

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -10,4 +10,14 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    protected Address(){
+        // 외부에서 new Address() 로 생성하는 것을 막기 위함
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
